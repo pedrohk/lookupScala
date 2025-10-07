654b108a0cbb27023d6c9219c3de9cd2de4033f5
Pedro <pedrohk@gmail.com>
2025-10-07 11:48:51 -0300

    adding tests

--- src/test/scala/DPKTest.scala
+++ src/test/scala/DPKTest.scala
@@ -1,6 +1,21 @@
 import org.scalatest.funsuite.AnyFunSuite
+import org.scalatest.matchers.should.Matchers
 
-class DPK01Test extends AnyFunSuite {
+class DPK01Test extends AnyFunSuite with Matchers {
+  test("1. Basic Bidirectional Map") {
+    val idToName = Map(1 -> "John")
+    val nameToEmail = Map("John" -> "john@john.jhon.com")
+    val emailToName = Map("john@john.jhon.com" -> "John")
 
- 
+    def lookup(key: Any): Option[String] = key match {
+      case id: Int => idToName.get(id)
+      case name: String if nameToEmail.contains(name) => nameToEmail.get(name)
+      case email: String => emailToName.get(email)
+    }
+
+    lookup(1) shouldBe Some("John")
+    lookup("John") shouldBe Some("john@john.jhon.com")
+    lookup("john@john.jhon.com") shouldBe Some("John")
+    lookup(99) shouldBe None
+  }
 }
